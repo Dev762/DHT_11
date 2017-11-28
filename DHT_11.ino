9d1edccf762e162ac809d7518816f7107ba5b792
Devendra Singh Shekhawat <shekhawat.dev313@gmail.com>
2017-11-28 10:52:51 -0800

    Update DHT_11.ino

--- DHT_11.ino
+++ DHT_11.ino
@@ -1,20 +1,38 @@
 #include <LiquidCrystal.h>
+#include "DHT.h"
+
+// set the DHT Pin
+#define DHTPIN 8
+
 // initialize the library with the numbers of the interface pins
-LiquidCrystal lcd(8, 9, 4, 5, 6, 7); /// REGISTER SELECT PIN,ENABLE PIN,D4 PIN,D5 PIN, D6 PIN, D7 PIN
-void setup()
-{
-  // set up the LCD's number of columns and rows:
-lcd.begin(16, 2);
-Serial.begin(9600);
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  // set up the LCD's number of columns and rows: 
+  lcd.begin(16, 2);
+  dht.begin();
+  
+  // Print a message to the LCD.
+  lcd.print("Temp:  Humidity:");
 }
-void loop()
-{
-// set the cursor to column 0, line 1
-lcd.print("   YOUR NAME ");      //print name
-lcd.setCursor(0, 1);        // set the cursor to column 0, line 2
-lcd.print("YOUR CLASS");      //print class
-Serial.println("hello");
-delay(750);         //delay of 0.75sec
-//lcd.scrollDisplayLeft();      //shifting data on LCD
-lcd.setCursor(0, 0);        // set the cursor to column 0, line1
-}   // Code End
+
+void loop() {
+  delay(500);
+  // set the cursor to column 0, line 1
+  lcd.setCursor(0, 1);
+  // read humidity
+  float h = dht.readHumidity();
+  //read temperature in Fahrenheit
+  float f = dht.readTemperature(true);
+
+  if (isnan(h) || isnan(f)) {
+    lcd.print("ERROR");
+    return;
+  }
+
+  lcd.print(f);
+  lcd.setCursor(7,1);
+  lcd.print(h);  
+}
